c7697d1af312440fb6bb81a1f135a0d0cd255909
Sefdine <74672023+Sefdine@users.noreply.github.com>
2024-04-12 14:12:58 +0000

    change airflow dag runner

--- app/AirFlow/dag_runner.py
+++ app/AirFlow/dag_runner.py
@@ -18,10 +18,50 @@ dag = DAG(
     schedule_interval=timedelta(days=1),
 )
 
-run_script = BashOperator(
-    task_id='run_engagement_script',
-    bash_command='python3 /home/like/app/MapReduce/main.py /home/like/app/Data/mastodon_data.json',
+# ********** User Engagement ************
+
+user_engagement = BashOperator(
+    task_id='user_engagement',
+    bash_command='python3 /home/like/app/MapReduce/UserEngagement/engagement.py /home/like/app/Data/mastodon_data.json',
     dag=dag,
 )
 
-run_script
+user_followers = BashOperator(
+    task_id='user_followers',
+    bash_command='python3 /home/like/app/MapReduce/UserEngagement/followers.py /home/like/app/Data/mastodon_data.json',
+    dag=dag,
+)
+
+user_most_used_tag = BashOperator(
+    task_id='user_most_used_tag',
+    bash_command='python3 /home/like/app/MapReduce/UserEngagement/mostUsedTag.py /home/like/app/Data/mastodon_data.json',
+    dag=dag,
+)
+
+user_growth_over_time = BashOperator(
+    task_id='user_growth_over_time',
+    bash_command='python3 /home/like/app/MapReduce/UserEngagement/userGrowthOverTime.py /home/like/app/Data/mastodon_data.json',
+    dag=dag,
+)
+
+# *********** Content Popularity **************
+
+content_language_script = BashOperator(
+    task_id='content_language_script',
+    bash_command='python3 /home/like/app/MapReduce/ContentPopularity/languagePostCategorize.py /home/like/app/Data/mastodon_data.json',
+    dag=dag,
+)
+
+content_multimedia_script = BashOperator(
+    task_id='content_multimedia_script',
+    bash_command='python3 /home/like/app/MapReduce/ContentPopularity/postMultimediaCount.py /home/like/app/Data/mastodon_data.json',
+    dag=dag,
+)
+
+content_shared_website = BashOperator(
+    task_id='content_shared_website',
+    bash_command='python3 /home/like/app/MapReduce/ContentPopularity/sharedWebsite.py /home/like/app/Data/mastodon_data.json',
+    dag=dag,
+)
+
+user_engagement >> user_followers >> user_most_used_tag >> user_growth_over_time >> content_language_script >> content_multimedia_script >> content_shared_website
